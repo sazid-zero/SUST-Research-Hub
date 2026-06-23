aa27e97bfa60e4fb7888403848e9bc3903c1e1b9
sazid-zero <sharif.sazid.3@gmail.com>
2026-06-23 20:58:21 +0600

    update

--- backups/scratch_introspect.js
+++
@@ -1,1 +0,0 @@
-const { db } = require('./lib/db/index.ts'); // Need to use ts-node or similar. Wait, better yet, I'll just write a JS file using `pg` if I have the URL.
--- backups/test-schema.ts
+++
@@ -1,5 +0,0 @@
-import { neon } from '@neondatabase/serverless';
-import * as dotenv from 'dotenv';
-dotenv.config({path: '.env.local'});
-const sql = neon(process.env.DATABASE_URL);
-sql`SELECT column_name, data_type, is_nullable FROM information_schema.columns WHERE table_name = 'thesis_authors'`.then(console.log);
